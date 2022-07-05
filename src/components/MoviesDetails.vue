7476f596b463e8503c6f7095c22e62d34bf540c3
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-07-05 10:05:11 +0200

    ajout bande annonce dans movie details

--- src/components/MoviesDetails.vue
+++ src/components/MoviesDetails.vue
@@ -5,23 +5,26 @@
                 <img v-bind:src="preUrlImg + movie.poster_path" alt="moviePoster" id="MoviePoster" />
             </div>
             <div class="col-lg-6 col-md-12 col-sm-12 m-auto description">
-                <img v-bind:src="preUrlImg + movie.video" alt="movieVideo" id="MovieVideo" />
                 <br>
                 <h4 class="title text-center text-uppercase"><b>{{ movie.title }}</b></h4>
+                <div class="mt-5" id="trailer" v-if="video[0]">
+                    <h4 class="text-start fs-6"><b>Regarder la bande annonce :</b></h4>
+                    <iframe width="650" height="300" v-bind:src="'https://www.youtube.com/embed/' + video[0].key"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen class="shadow">
+                    </iframe>
+                </div>
                 <br>
-                <h4 class="text-start fs-6"><b>Date de sortie :</b> {{ movie.release_date }}</h4>
-                <br>
-                <h4 class="text-start fs-6"><b>Note moyenne :</b> {{ movie.vote_average }}/10</h4>
-                <br>
-                <h4 class="text-start fs-6"><b>Titre original :</b> {{ movie.original_title }}</h4>
-                <br>
-                <h4 class="text-start fs-6 text-uppercase"><b class="text-capitalize">Langue :</b> {{ movie.original_language }}</h4>
-                <br>
-                <h4 class="text-start fs-6"><b>Résumé :</b> {{ movie.overview }}</h4>
-                <br>
-                <h4 class="text-start fs-6"><b>Budget :</b> {{ movie.budget }} <b>$</b></h4>
-                <br>
-                <h4 class="text-start fs-6"><b>Durée du film :</b> {{ movie.runtime }} minutes</h4>
+                <h4 class="text-start fs-6 py-1"><b>Date de sortie :</b> {{ movie.release_date }}</h4>
+                <h4 class="text-start fs-6 py-1"><b>Note moyenne :</b> {{ movie.vote_average }}/10</h4>
+                <h4 class="text-start fs-6 py-1"><b>Titre original :</b> {{ movie.original_title }}</h4>
+                <h4 class="text-start fs-6 text-uppercase py-1"><b class="text-capitalize">Langue :</b> {{
+                        movie.original_language
+                }}</h4>
+                <h4 class="text-start fs-6 py-1"><b>Résumé :</b> {{ movie.overview }}</h4>
+                <h4 class="text-start fs-6 py-1"><b>Budget :</b> {{ movie.budget }} <b>$</b></h4>
+                <h4 class="text-start fs-6 py-1"><b>Durée du film :</b> {{ movie.runtime }} minutes</h4>
             </div>
         </div>
     </div>
@@ -35,10 +38,10 @@ export default {
     data() {
         return {
             movie: [],
+            video: [],
             loading: true,
             errored: false,
             preUrlImg: "https://image.tmdb.org/t/p/original/",
-            preUrlVideo: "https://video.tmdb.org/t/p/original",
             movieId: this.$route.params.id
         }
     },
@@ -49,12 +52,18 @@ export default {
                 this.movie = response.data;
                 console.log(this.movie)
             })
-    }
+        axios
+            .get("https://api.themoviedb.org/3/movie/" + this.movieId + "/videos?api_key=8d2265a50d4907bf6dd28e4ad308b47e&language=fr")
+            .then((response) => {
+                this.video = response.data.results;
+                console.log(this.video);
+            })
+    },
 }
 </script>
 
 <style>
-#MoviePoster{
+#MoviePoster {
     height: 700px;
     width: 500px;
 }
@@ -63,18 +72,17 @@ export default {
     font-family: 'Yellowtail', cursive;
 }
 
- @media screen and (min-width: 768px) and (max-width: 991px) {
-    #MoviePoster{
+@media screen and (min-width: 768px) and (max-width: 991px) {
+    #MoviePoster {
         height: 600px;
         width: 400px;
     }
- }
+}
 
- @media screen and (max-width: 767px) {
-    #MoviePoster{
+@media screen and (max-width: 767px) {
+    #MoviePoster {
         height: 400px;
         width: 300px;
     }
- }
-
+}
 </style>
