b9c600daf4ae1a6d5ddaff1be79b2afb69ffe39d
Annox <63057879+DylanBrss@users.noreply.github.com>
2022-07-06 16:55:38 +0200

    changement

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-module.exports = defineConfig({ 
-  publicPath: process.env.NODE_ENV === "production" ? "/Cinerama/" : "/", 
+module.exports = defineConfig({
+   transpileDependencies: true,
 });
 
 
